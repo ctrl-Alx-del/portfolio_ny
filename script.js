93f8c5375018136fc36b56d6d97c1b3e3007c5fd
Alexander <electromanati@gmail.com>
2023-05-21 18:05:32 +0200

    burger menu implemented

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let mobileIsTrue = false;
+
+let screenSize = window.innerWidth;
+
+if (screenSize < 350) {
+  mobileIsTrue = true;
+}
+
 //lav så teksten gradvist bliver fra lines til text ligesom typewriteren
 
 //lav en charlie tango style transition. Kinda Done
@@ -8,11 +16,36 @@ h2.addEventListener("mouseenter", showFullLogo);
 function showFullLogo() {
   allSpans = document.querySelectorAll(".hidden");
   allSpans.forEach((element) => {
-    element.style.display = "inline";
+    if (mobileIsTrue === false) {
+      element.style.display = "inline";
+      const navigationBar = document.querySelector(".ulNav");
+      navigationBar.style.marginRight = "558px";
+    }
   });
+}
 
-  const navigationBar = document.querySelector(".ulNav");
-  navigationBar.style.marginRight = "558px";
+const burgerMenu = document.querySelector(".burger");
+burgerMenu.addEventListener("click", toggleMenu);
+const menu = document.querySelector(".ulNav");
+
+//burger menu activation
+function toggleMenu() {
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+
+  //arbejd på så den lukker når man klikker linksne!
+  const menuLists = document.querySelectorAll(".ulNav li");
+  menuLists.forEach((listItem) => {
+    listItem.addEventListener("clicked", () => {
+      const clickedList = true;
+      if (clickedList) {
+        menu.style.display = "none";
+      }
+    });
+  });
 }
 
 //fortsæt med at lave videre på det nedenunder så den skifter position i gridet og fader indtil den når midten!
